66731ffcd87a621f67ddb743062f7dd7cdeab320
xachiperadze <xachiperadevasili@gmail.com>
2024-05-10 20:42:33 +0400

    commit

---
+++ day 01/homework/00.py
@@ -0,0 +1,43 @@
+name = "levani"
+# არის ცვლადი
+# = ცვლადისთვის მნიშვნელობის მინიჭებული სიმბოლო
+# "levani" არის ცვლადისთვის მნიშვნელობა
+
+surname = "khachiperadze"
+
+#print(name)
+#პრინტ უნქციას გადაეცემა ეკრანზე გამოსატანი ცვლადი ან სტრინგი
+
+name = "levani"  # ეს არის str (string) ტიპის ცვლადი
+age = 10  # ეს არის int ( integer ) მთელი რიცხვი
+height = 152.0  # ეს არის Float ტიპის ცვლადი ( ათწილადი )
+#boolean  (bool) ტიპის ცვლადი
+
+knows_programming = True #true ან False
+is_ugly = False    #snakecose(უბრალოდ წერის სტილი სტანდარტულად)   
+
+isugly = False #ჯავასკრიპტული camelcase
+
+
+print(name + " " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print(name + str(age))
+
+
+
+
+
+name = "levani" 
+surname = "khachiperadze"
+age = 10
+height = 152.0
+print(name + str(age))
